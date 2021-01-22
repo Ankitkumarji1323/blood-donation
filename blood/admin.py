105779124c654ca9a4132002f4b12b59611c90f1
Sagor <mbrsagor@gmail.com>
2021-01-22 22:12:49 +0600

    Admin template added fields

--- blood/admin.py
+++ blood/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
 
-# Register your models here.
+from blood.models.location import Location
+from blood.models.donor import Donor
+
+
+class LocationAdmin(admin.ModelAdmin):
+    list_display = ['id', 'name', 'parent', 'is_active']
+    search_fields = ['name', 'parent']
+
+admin.site.register(Location, LocationAdmin)
+
+
+class DonorAdmin(admin.ModelAdmin):
+    list_display = ['id', 'first_name', 'phone_number', 'select_blood', 'last_donation_date', 'gender']
+    search_fields = ['first_name', 'phone_number', 'select_blood', 'last_donation_date']
+    edit_fields = ['phone_number', 'select_blood']
+    link_display = ['id', 'first_name']
+    
+admin.site.register(Donor, DonorAdmin)
--- blood/models/donor.py
+++ blood/models/donor.py
@@ -6,7 +6,7 @@ from blood.utils import SelectGender, BloodGroup
 
 
 class Donor(DomainEntity):
-    fist_name = models.CharField(max_length=40)
+    first_name = models.CharField(max_length=40)
     last_name = models.CharField(max_length=40)
     address = models.TextField()
     current_location = models.ForeignKey(Location, on_delete=models.CASCADE, blank=True, related_name='donor_location')
@@ -16,10 +16,12 @@ class Donor(DomainEntity):
     date_of_birth = models.DateField(null=True)
     gender = models.IntegerField(choices=SelectGender.select_gender(), default=SelectGender.MALE.value)
     profession = models.CharField(max_length=100, blank=True, null=True)
+    last_donation_date = models.DateField(null=True)
 
 
     def __str__(self):
-        return self.first_name
+        return self.first_name[:20]
     
     def get_full_name(self):
         return f"{self.first_name} {self.last_name}"
+    
