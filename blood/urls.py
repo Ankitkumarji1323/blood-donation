bbdc005205fe1a545080caf41448bbf9ccff7410
Sagor <mbrsagor@gmail.com>
2021-01-25 02:13:36 +0600

    Login and registrtion static page created

--- blood/urls.py
+++ blood/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
 from blood.views.dashboard_view import Dashboard
 from blood.views.homepage_views import Homepage
+from blood.views.auth_views import LoginView, RegistrationView
 
 urlpatterns = [
     path('', Homepage.as_view(), name="homepage"),
     path('dashboard/', Dashboard.as_view(), name="dashboard"),
+    path('login/', LoginView.as_view(), name="login"),
+    path('registration/', RegistrationView.as_view(), name="registration"),
 ]
