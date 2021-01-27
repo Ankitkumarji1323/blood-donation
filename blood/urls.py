05d2ecba3aba037f85e57b0c2a98b8a278569ee8
Sagor <mbrsagor@gmail.com>
2021-01-28 01:23:03 +0600

    User login done

--- blood/urls.py
+++ blood/urls.py
@@ -1,11 +1,11 @@
 from django.urls import path
 from blood.views.dashboard_view import Dashboard
 from blood.views.homepage_views import Homepage
-from blood.views.auth_views import LoginView, RegistrationView
+from blood.views.auth_views import Login, SingUpView
 
 urlpatterns = [
     path('', Homepage.as_view(), name="homepage"),
     path('dashboard/', Dashboard.as_view(), name="dashboard"),
-    path('login/', LoginView.as_view(), name="login"),
-    path('registration/', RegistrationView.as_view(), name="registration"),
+    path('login/', Login.as_view(), name="login"),
+    path('registration/', SingUpView.as_view(), name="registration"),
 ]
--- blood/views/auth_views.py
+++ blood/views/auth_views.py
@@ -1,9 +1,42 @@
-from django.views.generic import TemplateView
+from django.contrib.auth.views import LoginView
+from django.contrib.auth import login, logout
+from django.shortcuts import resolve_url
+from django.views import View
+from django.shortcuts import redirect
+from django.urls import reverse_lazy
+from django.views.generic.edit import CreateView
+
+from blood.form.login_form import CustomAuthenticationForm
 
 
-class LoginView(TemplateView):
+class Login(LoginView):
+    authentication_form = CustomAuthenticationForm
+    form_class = CustomAuthenticationForm
+    redirect_authenticated_user = False
     template_name = 'auth/login.html'
 
+    def get_success_url(self):
+        url = self.get_redirect_url()
+        return url or resolve_url('/dashboard/')
 
-class RegistrationView(TemplateView):
+    def form_valid(self, form):
+        remember_me = form.cleaned_data['remember_me']
+        login(self.request, form.get_user())
+
+        if remember_me:
+            self.request.session.set_expiry(1209600)
+
+        return super(Login, self).form_valid(form)
+
+
+class Logout(View):
+
+    def get(self, request):
+        logout(request)
+        return redirect('/login/')
+
+
+class SingUpView(CreateView):
+    # form_class = CustomUserCreationForm
+    success_url = reverse_lazy('login')
     template_name = 'auth/registration.html'
