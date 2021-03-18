f4c3dca5628a14fec967a063fd6fab540bd11ad4
Md.Bozlur Rosid Sagor <39002174+mbrsagor@users.noreply.github.com>
2021-03-18 23:10:23 +0600

    status calculate

---
+++ blood/models/status.py
@@ -0,0 +1,13 @@
+import request
+
+def makeChange(request, chance):
+  
+  return chance * 2
+
+
+def sub(request, first_num, second_num):
+  if first_num:
+      return first_num / second_num
+   else:
+      return 0
+ 
