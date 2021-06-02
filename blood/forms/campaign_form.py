460bda10252d75b7141eeffa1e5fd9db4b2f6159
mbrsagor <mbrsagor@gmail.com>
2021-06-02 20:41:03 +0600

    Campagin forms added

---
+++ blood/forms/campaign_form.py
@@ -0,0 +1,21 @@
+from django import forms
+from django.forms import TextInput, NumberInput, DateTimeInput, FileInput
+
+from blood.models.campaign import Campaign
+
+
+class RecentDonorFrom(forms.ModelForm):
+    class Meta:
+        model = Campaign
+        fields = '__all__'
+
+        widgets = {
+            'campaign_name': TextInput(attrs={'class': 'form-control', 'id': 'campaign_name'}),
+            'day_name': TextInput(attrs={'class': 'form-control', 'id': 'day_name'}),
+            'sponsor_by': TextInput(attrs={'class': 'form-control', 'id': 'sponsor_by'}),
+            'address': TextInput(attrs={'class': 'form-control', 'id': 'address'}),
+            'material_cost': NumberInput(attrs={'class': 'form-control', 'id': 'material_cost'}),
+            'other_cost': NumberInput(attrs={'class': 'form-control', 'id': 'other_cost'}),
+            'picture': FileInput(attrs={'class': 'form-control', 'id': 'picture'}),
+            'campaign_date': DateTimeInput(attrs={'class': 'form-control', 'id': 'campaign_date', 'type': 'date'}),
+        }
--- blood/models/campaign.py
+++ blood/models/campaign.py
@@ -7,10 +7,11 @@ class Campaign(DomainEntity):
     campaign_name = models.CharField(max_length=120)
     day_name = models.CharField(max_length=35)
     sponsor_by = models.CharField(max_length=150, blank=True, null=True)
-    location = models.CharField(max_length=200)
+    address = models.CharField(max_length=200)
     material_cost = models.IntegerField(default=0)
     other_cost = models.IntegerField(default=0)
-    date = models.DateTimeField(blank=True, null=True)
+    campaign_date = models.DateTimeField(blank=True, null=True)
+    picture = models.ImageField(upload_to='Campaign', blank=True, null=True)
 
     def __str__(self):
         return self.campaign_name[:30]
